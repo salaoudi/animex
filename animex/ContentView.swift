0adb326e2d284bb028759eab0f43095d38956399
SALEH ALAOUDI <79456369+salaoudi@users.noreply.github.com>
2022-04-18 03:59:32 -0700

    Initial Commit

---
+++ animex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  animex
+//
+//  Created by Zayn on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ animex/animexApp.swift
@@ -0,0 +1,17 @@
+//
+//  animexApp.swift
+//  animex
+//
+//  Created by Zayn on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct animexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
